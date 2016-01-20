46ec16a8ec67fbe47736a1c2e620b3ba9c95ba93
Bornpagna <Bornpagna@users.noreply.github.com>
2016-01-20 19:36:33 +0700

    syheng commit guesthouse

---
+++ application/modules/room/controllers/FloorController.php
@@ -0,0 +1,30 @@
+<?php
+class Room_FloorController extends Zend_Controller_Action {
+	
+    public function init()
+    {    	
+    	header('content-type: text/html; charset=utf8');
+	}
+
+    public function indexAction()
+    {
+    	
+    }
+    public function addAction()
+    {
+    	 
+    }
+    public function editAction()
+    {
+    	 
+    }
+    
+	function itemAction()
+	{
+		$item= new Accounting_Form_Frmitem();
+		$frm_item=$item->Frmitem();
+		Application_Model_Decorator::removeAllDecorator($frm_item);
+		$this->view->frm_item = $frm_item;
+	}	
+   
+}
---
+++ application/modules/room/controllers/indexController.php
@@ -0,0 +1,80 @@
+<?php
+class Room_indexController extends Zend_Controller_Action {
+	protected $tr;
+	const REDIRECT_URL ='/room';
+    public function init()
+    {    	
+    	header('content-type: text/html; charset=utf8');
+    	$this->tr=Application_Form_FrmLanguages::getCurrentlanguage();
+    	defined('BASE_URL')	|| define('BASE_URL', Zend_Controller_Front::getInstance()->getBaseUrl());
+	}
+
+    public function indexAction()
+    {
+    	try{
+    		$db = new Room_Model_DbTable_DbRoomType();
+//     		if($this->getRequest()->isPost()){
+//     			$search=$this->getRequest()->getPost();
+//     		}
+//     		else{
+//     			$search = array(
+//     					'adv_search' => '',
+//     					'search_status' => -1,
+//     					'start_date'=> date('Y-m-01'),
+//     					'end_date'=>date('Y-m-d'));
+//     		}
+    		$rs_rows= $db->getAllRoomType();
+    		$glClass = new Application_Model_GlobalClass();
+    		$rs_rows = $glClass->getImgActive($rs_rows, BASE_URL, true);
+    		$list = new Application_Form_Frmtable();
+    		$collumns = array("ROOM_TYPE","STATUS");
+    		$link=array(
+    				'module'=>'room','controller'=>'index','action'=>'edit',
+    		);
+    		$this->view->list=$list->getCheckList(0, $collumns, $rs_rows,array('type'=>$link));
+    	}catch (Exception $e){
+    		Application_Form_FrmMessage::message("Application Error");
+    		Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
+    	}
+    }
+    public function addAction()
+    {
+    	if($this->getRequest()->isPost()){
+    		$_data = $this->getRequest()->getPost();
+    		try {
+    			$db = new Room_Model_DbTable_DbRoomType();
+    			if(!empty($_data['save_new'])){
+    				 $db->addRooomType($_data);
+    				Application_Form_FrmMessage::message($this->tr->translate('INSERT_SUCCESS'));
+    			}else{
+    				$db->addRooomType($_data);
+    				Application_Form_FrmMessage::Sucessfull($this->tr->translate('INSERT_SUCCESS'), self::REDIRECT_URL . '/index/index');
+    			}
+    		} catch (Exception $e) {
+    			Application_Form_FrmMessage::message($this->tr->translate('INSERT_FAIL'));
+    			$err =$e->getMessage();
+    			Application_Model_DbTable_DbUserLog::writeMessageError($err);
+    		}
+    	}	 
+    }
+    public function editAction()
+    {
+    	if($this->getRequest()->isPost()){
+    		$_data = $this->getRequest()->getPost();
+    		try {
+    			$db = new Room_Model_DbTable_DbRoomType();
+    			if(!empty($_data['save_close'])){
+    				$db->updateRoomType($_data);
+    				Application_Form_FrmMessage::Sucessfull($this->tr->translate('INSERT_SUCCESS'), self::REDIRECT_URL . '/index/index');
+    			}
+    		} catch (Exception $e) {
+    			Application_Form_FrmMessage::message($this->tr->translate('INSERT_FAIL'));
+    			$err =$e->getMessage();
+    			Application_Model_DbTable_DbUserLog::writeMessageError($err);
+    		}
+    	}
+    	$id=$this->getRequest()->getParam('id');
+    	$db = new Room_Model_DbTable_DbRoomType();
+    	$this->view->row_room_type=$db->getRoomTypeById($id);
+    }
+}
